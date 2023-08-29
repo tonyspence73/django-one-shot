499dd6ab4acba496bd0c270cf9d1dde4f7271016
Anthony Spence <anthonyspence08091973@gmail.com>
2023-08-29 15:50:37 -0700

    Feature 8 Complete

--- todos/views.py
+++ todos/views.py
@@ -3,8 +3,13 @@ from todos.models import TodoList
 
 
 def todo_list_list(request):
-    todo_list = get_object_or_404(TodoList)
+    todo_list = TodoList.objects.all()
     context = {
         "todo_list": todo_list,
     }
     return render(request, "todos/list.html", context)
+
+
+def todo_list_detail(request, id):
+    todolist = get_object_or_404(TodoList, id=id)
+    return render(request, "todos/detail.html", {"todolist": todolist})
